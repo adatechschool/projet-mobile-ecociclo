98b85152f3028a67186ebd25bb9835fbe2ccbcb7
Elodieguay <e.guayroso@gmail.com>
2023-08-31 20:23:46 +0200

    first route

---
+++ ecociclo/src/screens/LoadHome.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+
+const LoadHome = () => {
+  
+    
+  return (
+    <View style={styles.container}>
+        <Text>
+            ECOCICLO
+        </Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff2',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+export default LoadHome
+
